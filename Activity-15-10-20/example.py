18913371c3e9fbdfa495a20d082fb5477b3560d4
Estiven Velez <estivenvelez2002@gmail.com>
2020-10-15 22:42:00 -0500

    the solution of point 1 was uploaded

---
+++ Activity-15-10-20/example.py
@@ -0,0 +1,12 @@
+# #First Example 
+
+# b = float(input('Enter base of the triangle: '))
+# h = float(input('Enter height of the triangle: '))
+
+# #Calculation 
+
+# r = (b*h)/2
+
+# #Print 
+
+# print('The area of triangle is: ' , r)
